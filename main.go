9f3eff6083ce86639508722d2a473e567d366d8c
Andreas Schubert <andy.schubert@outlook.com>
2026-03-25 20:35:45 +0100

    boilerplate for http server

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("Port is not found in environment!")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		MaxAge:         300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/error", handlerError)
+
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Server starting on port %v", port)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Port:", port)
+}
